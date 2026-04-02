83c1fcc582949e2df2d7e97215233c34f489d4a2
corderojavierc <corderojavierc@gmail.com>
2026-04-02 12:16:23 +0200

    feat: added auth api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types=1);
+
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function (): void {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('clear', [AuthController::class, 'clear'])->name('clear');
+});
